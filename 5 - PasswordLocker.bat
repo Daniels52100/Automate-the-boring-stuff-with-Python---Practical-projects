efb3260e0ea07482699aa955b8b7a08ab1b85bbe
gabrielmsollero <42558274+gabrielmsollero@users.noreply.github.com>
2018-11-19 22:08:11 -0200

    Add files via upload

---
+++ 5 - PasswordLocker.bat
@@ -0,0 +1,2 @@
+@py.exe "C:\Users\Gabriel\Documents\UFU\Cursos e Livros\Livros\Automatize Boring Stuff\Practice Projects\5 - PasswordLocker.py" %*
+@pause
---
+++ 5 - PasswordLocker.py
@@ -0,0 +1,29 @@
+#! python3
+# This program saves passwords and sends to the clipboard the desired password when executed.
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python PasswordLocker.py [account] - copy account password')
+    sys.exit()
+
+if sys.argv[1] == 'new':
+    print('Insert account name (Or nothing to quit):')
+    newAccount = input()
+    print('Insert the password. (I won\'t look, I promise!)')
+    newPassword = input()
+    PASSWORDS[newAccount] = newPassword
+    print('Password successfully registered.')
+    # Because we didn't use actual files in this program, the new password won't be saved.
+    # We would need a file containing the PASSWORDS dictionary to save it, and open the file in the program.
+    # The objective with this feature is to test the argv's logic.
+else:
+    account = sys.argv[1] # sys.argv[0] is always a string containing the program's filename.
+    if account in PASSWORDS:
+        pyperclip.copy(PASSWORDS[account])
+        print('Password for ' + account + ' copied to clipboard.')
+    else:
+        print('There is no account named ' + account + '. To register it, execute the program with \'new\' as argument')
