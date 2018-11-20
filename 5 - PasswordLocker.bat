30232c20b38756d58afa110dda229a53963cfcdf
gabrielmsollero <42558274+gabrielmsollero@users.noreply.github.com>
2018-11-20 14:34:46 -0200

    Update 5 - PasswordLocker.bat

--- 5 - PasswordLocker.bat
+++ 5 - PasswordLocker.bat
@@ -1,3 +1,3 @@
-# You must change the line below to the complete path to your .py file.
+rem You must change the line below to the complete path to your .py file.
 @py.exe "C:\Users\Gabriel\Documents\UFU\Cursos e Livros\Livros\Automatize Boring Stuff\Practice Projects\5 - PasswordLocker.py" %*
 @pause
