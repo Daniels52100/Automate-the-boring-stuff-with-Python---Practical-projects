f853eb417bc722415d4dc71b6b7f7e6be752f336
gabrielmsollero <42558274+gabrielmsollero@users.noreply.github.com>
2018-11-20 14:07:46 -0200

    Update 8 - PhoneAndEmailFinder.bat

--- 8 - PhoneAndEmailFinder.bat
+++ 8 - PhoneAndEmailFinder.bat
@@ -1,2 +1,3 @@
+# You must change the line below to the complete path to your .py file.
 @py.exe "C:\Users\Gabriel\Documents\UFU\Cursos e Livros\Livros\Automatize Boring Stuff\Practice Projects\8 - PhoneAndEmailFinder.py" %*
 @pause
